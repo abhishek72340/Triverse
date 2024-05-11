ed612f4b06798601f1148e788a68818cf4958abd
abhishek <abhisingh.72340@gmail.com>
2024-05-11 23:07:11 +0530

    add calculate section

--- src/Components/CalculationSection/CalculationSection.jsx
+++ src/Components/CalculationSection/CalculationSection.jsx
@@ -1,5 +1,4 @@
 import "./CalculateSection.css";
-import React from "react";
 const CalculationSection = () => {
   return (
     <>
@@ -7,6 +6,26 @@ const CalculationSection = () => {
         metamorphosis of child:
         <br /> the philosophy of welmont
       </p>
+      <div className="calculate-bubble"></div>
+
+      <div className="calculate-img-container">
+        <img src="/images/calculate1.png" alt="calculate1" />
+        <img src="/images/calculate2.png" alt="calculate2" />
+      </div>
+      <div className="calculate-name-container">
+        <div className="name-container">
+          <p>THE BRAIN</p>
+          <p>The essence of child</p>
+        </div>
+        <div className="name-container">
+          <p>THE WINGS</p>
+          <p>Environment of learning </p>
+        </div>
+        <div className="name-container">
+          <p>METAMORPHOSIS</p>
+          <p>The transformation of child</p>
+        </div>
+      </div>
     </>
   );
 };
